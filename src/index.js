2765eeda5cf8495622ea07857f7db648ba520ff1
Protanton <protanton@gmail.com>
2019-09-18 13:13:27 +0300

    feat: brackets solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const arr = str.split('');
+  start:
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let open = bracketsConfig[j][0];
+      let close = bracketsConfig[j][1];
+      if (arr[i] === open) {
+        if (arr[i + 1] === close) {
+          arr.splice(i, 2);
+          i -= 2;
+        } else {
+          continue start;
+        }
+      }
+    }
+  }
+  if (arr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
